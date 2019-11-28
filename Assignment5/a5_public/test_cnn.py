5a9b2d89f7ba65ad89303f5eb6b1b1a8a0a2b47f
Cheng W. Liu <chengwliu01@gmail.com>
2019-11-28 11:15:26 -0800

    added numpy testing code

--- Assignment5/a5_public/test_cnn.py
+++ Assignment5/a5_public/test_cnn.py
@@ -38,3 +38,44 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    # import torch
+    # import numpy
+    # from scipy.signal import correlate2d
+
+    # X_reshaped = np.array(
+    #     [[[-0.1117, -0.4966, 0.1631, -0.8817, 0.0539],
+    #       [0.6684, -0.0597, -0.4675, -0.2153, 0.8840]],
+    #      [[-0.7584, -0.3689, -0.3424, -1.4020, 1.4255],
+    #       [0.7987, -1.4949, 0.8810, -1.1786, -0.9340]],
+    #      [[-0.5675, -0.2772, -0.4030, 0.4195, 0.9380],
+    #       [0.0078, -0.3139, -1.1567, 1.8409, -1.0174]]]
+    # )
+    # batch_size = X_reshaped.shape[0]
+    # m_word = X_reshaped.shape[2]
+    #
+    # kernels = np.array([[[-0.0811, -0.4345],
+    #                      [0.3839, 0.3083]],
+    #
+    #                     [[0.2528, 0.3988],
+    #                      [0.1839, 0.2658]],
+    #
+    #                     [[0.4149, -0.1007],
+    #                      [-0.3900, -0.2459]],
+    #
+    #                     [[-0.0667, -0.0549],
+    #                      [-0.0034, 0.2865]]])
+    #
+    # f = kernels.shape[0]
+    # kernel_size = kernels.shape[2]
+    #
+    # X_conv = np.zeros((batch_size, f, m_word - kernel_size + 1))
+    #
+    # for b in range(batch_size):
+    #     for i in range(f):
+    #         X_conv[b, i, :] = correlate2d(X_reshaped[b, :, :], kernels[i, :, :], mode='valid')[0]
+    #
+    # X_conv = X_conv * (X_conv > 0)
+    #
+    # X_conv = np.amax(X_conv, axis=2)
+    #
--- Assignment5/a5_public/test_highway.py
+++ Assignment5/a5_public/test_highway.py
@@ -25,3 +25,22 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    # import torch
+    # import numpy
+    #
+    # X_conv_out = np.array([[-0.1549, -1.3706, -0.1319], [0.8848, -0.2611, 0.6104]])
+    #
+    # w_proj_weight = np.array([[-0.5439, -0.1133, -0.2773], [-0.1540, -0.5100, 0.2317], [-0.5175, -0.0368, 0.2007]])
+    # w_proj_bias = np.array([-0.1946, 0.3276, 0.0728]).reshape(-1, 1)
+    #
+    # X_proj = (np.matmul(w_proj_weight, X_conv_out.T) + w_proj_bias).T
+    # X_proj = X_proj * (X_proj > 0)
+    #
+    # gate_proj_weight = np.array([[0.3174, 0.3704, -0.2549], [0.2098, -0.2498, 0.1810], [-0.3017, 0.2671, 0.1169]])
+    # gate_proj_bias = np.array([-0.2259, -0.2832, 0.1494]).reshape(-1, 1)
+    #
+    # X_gate = (np.matmul(gate_proj_weight, X_conv_out.T) + gate_proj_bias).T
+    # X_gate = 1.0 / (1.0 + np.exp(-X_gate))
+    #
+    # X_highway = np.multiply(X_gate, X_proj) + np.multiply(1.0 - X_gate, X_conv_out)
